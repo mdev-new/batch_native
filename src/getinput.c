cdfb06a6be39c89adfaf9ea3bed42d939a10c0c5
MousieDev <MousieDev@users.noreply.github.com>
2022-11-03 16:20:13 +0100

    Wip try to set raster fonts, doesnt work rn

--- src/getinput.c
+++ src/getinput.c
@@ -26,6 +26,89 @@ const int UPS = 120; // Input polls per second
 const int WAIT_TIME = 1000 / UPS;
 signed wheelDelta;
 
+typedef struct {
+	BOOL isHooked;
+	void* FunctionAddress;
+	void* Hook;
+	char  Jmp[6];
+	char  APIBytes[6];
+	void* APIFunction;
+} hook_t;
+
+typedef HDC(*fBeginPaint)(HWND hWnd, LPPAINTSTRUCT lpPaintStruct);
+typedef unsigned __int64 QWORD;
+
+BOOL InitializeHook(hook_t* Hook, char* ModuleName, char* FunctionName, void* HookFunction)
+	{
+		HMODULE hModule;
+		ULONG OrigFunc, FuncAddr;
+		char opcodes[] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0xe9, 0x00, 0x00, 0x00, 0x00 };
+
+		if (Hook->isHooked) {
+			return FALSE;
+		}
+		hModule = GetModuleHandle(ModuleName);
+		if (hModule == NULL) {
+			Hook->isHooked = FALSE;
+			return FALSE;
+		}
+		Hook->FunctionAddress = GetProcAddress(hModule, FunctionName);
+		if (Hook->FunctionAddress == NULL) {
+			Hook->isHooked = FALSE;
+			return FALSE;
+		}
+		Hook->Jmp[0] = 0xe9;
+		*(QWORD*)&Hook->Jmp[1] = (QWORD)HookFunction - (QWORD)Hook->FunctionAddress - 5;
+		CopyMemory(Hook->APIBytes, Hook->FunctionAddress, 5);
+		Hook->APIFunction = VirtualAlloc(NULL, 4096, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+		if (Hook->APIFunction == NULL) {
+			return FALSE;
+		}
+		CopyMemory(Hook->APIFunction, Hook->APIBytes, 5);
+		OrigFunc = (QWORD)Hook->APIFunction + 5;
+		FuncAddr = (QWORD)Hook->FunctionAddress + 5;
+		*(LPBYTE)((LPBYTE)Hook->APIFunction + 5) = 0xe9;
+		*(QWORD*)((LPBYTE)Hook->APIFunction + 6) = (QWORD)FuncAddr - (QWORD)OrigFunc - 5;
+		Hook->isHooked = TRUE;
+		return TRUE;
+	}
+
+	BOOL InsertHook(hook_t* Hook)
+	{
+		DWORD op;
+		if (!Hook->isHooked) {
+			return FALSE;
+		}
+		VirtualProtect(Hook->FunctionAddress, 5, PAGE_EXECUTE_READWRITE, &op);
+		memcpy(Hook->FunctionAddress, Hook->Jmp, 5);
+		VirtualProtect(Hook->FunctionAddress, 5, op, &op);
+		return TRUE;
+	}
+
+	BOOL Unhook(hook_t* Hook)
+	{
+		DWORD op;
+		if (!Hook->isHooked) {
+			return FALSE;
+		}
+		VirtualProtect(Hook->FunctionAddress, 5, PAGE_EXECUTE_READWRITE, &op);
+		memcpy(Hook->FunctionAddress, Hook->APIBytes, 5);
+		VirtualProtect(Hook->FunctionAddress, 5, op, &op);
+
+		Hook->isHooked = FALSE;
+		return TRUE;
+	}
+
+	BOOL FreeHook(hook_t* Hook)
+	{
+		if (Hook->isHooked) {
+			return FALSE;
+		}
+		VirtualFree(Hook->APIFunction, 0, MEM_RELEASE);
+		memset(Hook, 0, sizeof(hook_t*));
+		return TRUE;
+	}
+
 CHAR b[21], *c;
 PCHAR itoa_(i,x) {
   c=b+21,x=abs(i);
@@ -74,6 +157,9 @@ DWORD CALLBACK Process(void *data) {
   HHOOK mouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookProc, NULL, 0);
   MSG mouseMsg;
 
+	// TODO set raster font
+	// 
+
   while(TRUE) {
     SetConsoleMode(hIn, ENABLE_EXTENDED_FLAGS & ~ENABLE_QUICK_EDIT_MODE);
     GetCurrentConsoleFont(hOut, FALSE, &info);
@@ -113,10 +199,28 @@ DWORD CALLBACK Process(void *data) {
   return TRUE;
 }
 
+hook_t Hook;
+fBeginPaint hookBp;
+HFONT hfnt;
+
+HDC HookBeginPaint(HWND hWnd, LPPAINTSTRUCT lpPaint) {
+	HDC origReturn = hookBp(hWnd, lpPaint);
+	MessageBox(NULL, "HookBeginPaint", "", MB_OK);
+	SelectObject(origReturn, hfnt);
+}
+
 BOOL APIENTRY DllMain(HINSTANCE hInst, DWORD dwReason, LPVOID lpReserved) {
   if (dwReason == DLL_PROCESS_ATTACH) {
 	DisableThreadLibraryCalls(hInst);
+	hfnt = CreateFont(-8, -8, 0, 0, 0, 0, 0, 0, OEM_CHARSET, OUT_DEVICE_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, L"Terminal");
+	InitializeHook(&Hook, "user32.dll", "BeginPaint", HookBeginPaint);
+
+	hookBp = (fBeginPaint)Hook.APIFunction;
+	InsertHook(&Hook);
     CloseHandle(CreateThreadS(Process));
-  }
+  }  else if(dwReason == DLL_PROCESS_DETACH) {
+		Unhook(&Hook);
+		FreeHook(&Hook);
+	}
   return TRUE;
 }
--- tools/comp.cmd
+++ tools/comp.cmd
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 :: del %temp%\getinput.dll %temp%\discord.dll
 
-gcc -shared -o %TEMP%\getinput.dll src\getinput.c -Isrc -std=gnu99 -m64 -msse4.1 -Os -flto -fno-pic -fno-pie -fno-plt -fvisibility=hidden -fcompare-debug-second -fno-exceptions -fno-stack-protector -fno-math-errno -fno-ident -fno-asynchronous-unwind-tables -nostartfiles -nodefaultlibs -nostdlib -nolibc -luser32 -lkernel32 -Wl,-s,-e,DllMain,--gc-sections,--reduce-memory-overheads,--no-seh,--disable-reloc-section,--build-id=none
+gcc -shared -o %TEMP%\getinput.dll src\getinput.c -Isrc -std=gnu99 -m64 -msse4.1 -Os -flto -fno-pic -fno-pie -fno-plt -fvisibility=hidden -fcompare-debug-second -fno-exceptions -fno-stack-protector -fno-math-errno -fno-ident -fno-asynchronous-unwind-tables -nostartfiles -nodefaultlibs -nostdlib -nolibc -luser32 -lkernel32 -lgdi32 -Wl,-s,-e,DllMain,--gc-sections,--reduce-memory-overheads,--no-seh,--disable-reloc-section,--build-id=none
 
 set sources= connection_win.cpp discord_register_win.cpp discord_rpc.cpp rpc_connection.cpp serialization.cpp
 g++ -shared -static-libgcc -static-libstdc++ -static -o %TEMP%\discord.dll src\discord.cpp %sources: = src/extern/discord-rpc/src/% -Isrc -Isrc\extern\discord-rpc\include -Isrc\extern\rapidjson\include -mconsole -m64 -Os -flto -fno-pic -fno-pie -fno-plt -fvisibility=hidden -fcompare-debug-second -fno-exceptions -fno-stack-protector -fno-math-errno -fno-ident -fno-asynchronous-unwind-tables -Wl,-s,-e,DllMain,--gc-sections,--reduce-memory-overheads,--no-seh,--disable-reloc-section,--build-id=none -DDISCORD_WINDOWS -DDISCORD_DISABLE_IO_THREAD -DDISCORD_DYNAMIC_LIB -DDISCORD_BUILDING_SDK
